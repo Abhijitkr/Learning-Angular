feadd2e953eb0e76055259254e95ed8db72aa3c4
Abhijitkr <abhijitkr600@gmail.com>
2021-09-24 00:39:53 +0530

    assignment 1 completed

--- basics-assignment-1-start/src/app/warning-alert/warning-alert.component.ts
+++ basics-assignment-1-start/src/app/warning-alert/warning-alert.component.ts
@@ -1,7 +1,11 @@
 import { Component } from '@angular/core';
  
 @Component({
-    selector: 'app-warning-alert',
-    templateUrl: './warning-alert.component.html'
+    selector: '[app-warning-alert]',
+    templateUrl: './warning-alert.component.html',
+    styles: [`
+    p{
+        color: red;
+    }`]
 })
 export class WarningAlertConponent{}
