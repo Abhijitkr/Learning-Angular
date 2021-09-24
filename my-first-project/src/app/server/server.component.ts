beb5d1181dc2d4c094225879c39985fce5e76d88
Abhijitkr <abhijitkr600@gmail.com>
2021-09-24 11:25:11 +0530

    Databinding - String Interpolation

--- my-first-project/src/app/server/server.component.ts
+++ my-first-project/src/app/server/server.component.ts
@@ -6,4 +6,11 @@ import { Component } from "@angular/core";
 })
 export class ServerComponent{
 
+    serverId: number = 20;
+    serverStatus: string = "online";
+
+    getServerStatus() {
+        return this.serverStatus;
+    }
+
 }
