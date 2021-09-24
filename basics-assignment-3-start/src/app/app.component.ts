c4ed5ce520eb4e55b7841649eebb3c8d7ab8005c
Abhijitkr <abhijitkr600@gmail.com>
2021-09-24 17:06:32 +0530

    toggel para

--- basics-assignment-3-start/src/app/app.component.ts
+++ basics-assignment-3-start/src/app/app.component.ts
@@ -6,4 +6,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+
+  count:number = 0;
+  numbs: Number[]= [];
+  showSecret = false;
+
+  addLogs(){
+    this.showSecret = !this.showSecret;
+    this.numbs.push(this.count);
+    this.count++;
+  }
 }
