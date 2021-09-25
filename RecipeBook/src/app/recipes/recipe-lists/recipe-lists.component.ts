3c2033877a78220629e926f753e54bf6d51a0ac5
Abhijitkr <abhijitkr600@gmail.com>
2021-09-25 13:07:16 +0530

    recipe detail hardcoded

---
+++ RecipeBook/src/app/recipes/recipe-lists/recipe-lists.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+
+import { Recipe } from '../recipe.model';
+
+@Component({
+  selector: 'app-recipe-lists',
+  templateUrl: './recipe-lists.component.html',
+  styleUrls: ['./recipe-lists.component.css']
+})
+export class RecipeListsComponent implements OnInit {
+
+  recipes: Recipe[] = [
+    new Recipe('A test recipe', 'This is only for testing', 'https://images.unsplash.com/photo-1542010589005-d1eacc3918f2?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1192&q=80'),
+    new Recipe('A test recipe', 'This is only for testing', 'https://images.unsplash.com/photo-1542010589005-d1eacc3918f2?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1192&q=80'),
+  ];
+  
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
