d5324299091845c6938a90b9595b2194d598fe16
Abhijitkr <abhijitkr600@gmail.com>
2021-09-28 14:35:11 +0530

    @ViewChild can add new Ingredient

--- RecipeBook/src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ RecipeBook/src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, EventEmitter, OnInit, Output, ViewChild } from '@angular/core';
+import { Ingredient } from 'src/app/shared/ingredient.model';
 
 @Component({
   selector: 'app-shopping-edit',
@@ -7,9 +8,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ShoppingEditComponent implements OnInit {
 
+  @ViewChild('nameInput') nameRef: ElementRef;
+  @ViewChild('amountInput') amountRef: ElementRef;
+  @Output() newIngredientAdded = new EventEmitter<Ingredient>();
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  onAddIngredient(){
+    const ingName: string = this.nameRef.nativeElement.value;
+    const ingAmount: number = this.amountRef.nativeElement.value;
+    const newIngredient = new Ingredient(ingName, ingAmount);
+    this.newIngredientAdded.emit(newIngredient);
   }
 
 }
