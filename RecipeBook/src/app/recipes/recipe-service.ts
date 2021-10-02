343bf73a6dae052d521664d7274d79ade188f989
Abhijitkr <abhijitkr600@gmail.com>
2021-10-02 12:31:59 +0530

    added ingredients list in recipe detail

--- RecipeBook/src/app/recipes/recipe-service.ts
+++ RecipeBook/src/app/recipes/recipe-service.ts
@@ -1,4 +1,5 @@
 import { EventEmitter } from "@angular/core";
+import { Ingredient } from "../shared/ingredient.model";
 
 import { Recipe } from "./recipe.model";
 
@@ -7,8 +8,8 @@ export class RecipeService {
     recipeSelected = new EventEmitter<Recipe>();
 
     private recipes: Recipe[] = [
-        new Recipe('A test recipe', 'This is only for testing', 'https://images.unsplash.com/photo-1542010589005-d1eacc3918f2?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1192&q=80'),
-        new Recipe('Another test recipe', 'This is only for testing', 'https://images.unsplash.com/photo-1542010589005-d1eacc3918f2?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1192&q=80'),
+        new Recipe('A test recipe', 'This is only for testing', 'https://images.unsplash.com/photo-1542010589005-d1eacc3918f2?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1192&q=80',[new Ingredient('Meat', 1), new Ingredient('bread', 2)]),
+        new Recipe('Another test recipe', 'This is only for testing', 'https://images.unsplash.com/photo-1542010589005-d1eacc3918f2?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1192&q=80', [new Ingredient('French Fries', 10), new Ingredient('Buns', 2)]),
     ];
 
     getRecipe(){
