4a5a3f03f0f167e2072271e79ebd13d802664a69
Abhijitkr <abhijitkr600@gmail.com>
2021-10-06 00:10:47 +0530

    Forms Template Driven Assignment Completed

--- forms-td-assignment-start/src/app/app.component.ts
+++ forms-td-assignment-start/src/app/app.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component} from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,26 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  defaultSubs = "Advanced";
+  wasSubmitted = false;
+  formData = {
+    email: "",
+    subscription: "",
+    pass: ""
+  }
 
+  onSubmit(form: NgForm) {
+
+    this.wasSubmitted = true;
+
+    this.formData.email = form.value.email;
+    this.formData.subscription = form.value.subscriptions;
+    this.formData.pass = form.value.password;
+
+    console.log(this.formData.email);
+    console.log(this.formData.subscription);
+    console.log(this.formData.pass);
+  }
 }
+
+
