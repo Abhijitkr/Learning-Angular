6ed3371408096edfbd1ec87e7b7a53e50cfcb4bb
Abhijitkr <abhijitkr600@gmail.com>
2021-10-08 15:28:52 +0530

    connected the form with form group

--- forms-reactive-assignment-start/src/app/app.component.ts
+++ forms-reactive-assignment-start/src/app/app.component.ts
@@ -1,9 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+
+  submitForm: FormGroup;
+
+  ngOnInit() {
+    this.submitForm = new FormGroup({
+      "projectName": new FormControl(null),
+      "email": new FormControl(null),
+      "projectStatus": new FormControl(null)
+    })
+  }
+
+  onSubmit() {
+    console.log(this.submitForm);
+    
+  }
 }
