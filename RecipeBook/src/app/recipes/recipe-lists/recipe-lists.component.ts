1083ac0a38e90644e82ed8c3f96f8fb07b449cd8
Abhijitkr <abhijitkr600@gmail.com>
2021-10-10 14:10:27 +0530

    delete ingredient from edit recipe list

--- RecipeBook/src/app/recipes/recipe-lists/recipe-lists.component.ts
+++ RecipeBook/src/app/recipes/recipe-lists/recipe-lists.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit} from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
+import { Subscription } from 'rxjs';
 
 import { RecipeService } from '../recipe-service';
 
@@ -10,22 +11,28 @@ import { Recipe } from '../recipe.model';
   templateUrl: './recipe-lists.component.html',
   styleUrls: ['./recipe-lists.component.css']
 })
-export class RecipeListsComponent implements OnInit {
+export class RecipeListsComponent implements OnInit, OnDestroy {
 
+  subscription: Subscription;
   recipes: Recipe[] = [];
 
   constructor(private recipeService: RecipeService, private router: Router, private route: ActivatedRoute) { }
 
   ngOnInit(): void {
-    this.recipeService.recipesChanged.subscribe(
+    this.subscription = this.recipeService.recipesChanged.subscribe(
       (recipes: Recipe[]) => {
         this.recipes = recipes;
       }
     );
     this.recipes = this.recipeService.getRecipes();
   }
-  
-  onNewRecipe(){
-    this.router.navigate(["new"], {relativeTo: this.route});
+
+  onNewRecipe() {
+    this.router.navigate(["new"], { relativeTo: this.route });
   }
+
+  ngOnDestroy() {
+    this.subscription.unsubscribe();
+  }
+
 }
