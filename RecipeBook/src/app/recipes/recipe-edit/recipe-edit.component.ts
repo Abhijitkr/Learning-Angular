37bf678cec1aa36eff7b07f4a91d114e2bc6eea6
Abhijitkr <abhijitkr600@gmail.com>
2021-10-10 13:00:13 +0530

    added ingredients in recipe edit form

--- RecipeBook/src/app/recipes/recipe-edit/recipe-edit.component.ts
+++ RecipeBook/src/app/recipes/recipe-edit/recipe-edit.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup } from '@angular/forms';
+import { FormArray, FormControl, FormGroup } from '@angular/forms';
 import { ActivatedRoute, Params } from '@angular/router';
 import { RecipeService } from '../recipe-service';
 
@@ -15,35 +15,52 @@ export class RecipeEditComponent implements OnInit {
 
   constructor(private route: ActivatedRoute, private recipeService: RecipeService) { }
 
-  ngOnInit(){
+  ngOnInit() {
     this.route.params.subscribe(
       (params: Params) => {
         this.id = +params['id'];
         this.editMode = params['id'] != null;
         this.initForm();
       }
-    )  
+    )
   }
 
-onSubmit(){
-  console.log(this.recipeForm);
-}
+  onSubmit() {
+    console.log(this.recipeForm);
+  }
 
-  private initForm(){
+  get controls(){
+    return (<FormArray>this.recipeForm.get('ingredients')).controls;
+  }
+
+  private initForm() {
     let recipeName = '';
     let recipeImagePath = '';
     let recipeDescription = '';
-    
-    if(this.editMode){
+    let recipeIngredients = new FormArray([]);
+
+    if (this.editMode) {
       const recipe = this.recipeService.getRecipe(this.id);
       recipeName = recipe.name;
       recipeImagePath = recipe.imagePath;
       recipeDescription = recipe.description;
+      if (recipe['ingredients']) {
+        for (let ingredient of recipe.ingredients) {
+          recipeIngredients.push(
+            new FormGroup({
+              'name': new FormControl(ingredient.name),
+              'amount': new FormControl(ingredient.amount)
+            })
+          );
+        }
+      }
     }
+
     this.recipeForm = new FormGroup({
       'name': new FormControl(recipeName),
       'imagePath': new FormControl(recipeImagePath),
-      'description': new FormControl(recipeDescription)
+      'description': new FormControl(recipeDescription),
+      'ingredients': recipeIngredients
     });
   }
 }
