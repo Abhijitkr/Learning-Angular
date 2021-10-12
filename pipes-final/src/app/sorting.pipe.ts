d14ae8419f9b62ed0440f8d4ad6714eb019ec873
Abhijitkr <abhijitkr600@gmail.com>
2021-10-12 17:14:24 +0530

    pipe assignment complete with custom sorting pipe

--- pipes-final/src/app/sorting.pipe.ts
+++ pipes-final/src/app/sorting.pipe.ts
@@ -1,12 +1,19 @@
 import { Pipe, PipeTransform } from '@angular/core';
 
 @Pipe({
-  name: 'sorting'
+  name: 'sorting',
+  pure: false
 })
 export class SortingPipe implements PipeTransform {
 
-  transform(value: string, filterStatus: string, propName: string): any {
-    if()
+  transform(value: any, propName: string): any {
+    return value.sort((a: any, b: any) => {
+      if(a[propName] > b[propName]){
+        return 1;
+      }else{
+        return -1;
+      }
+    })
   }
 
 }
