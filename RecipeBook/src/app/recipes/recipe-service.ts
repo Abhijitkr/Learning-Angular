21dea7f60ec0dbed69e00e880d5bc10a323088c8
Abhijitkr <abhijitkr600@gmail.com>
2021-10-17 22:25:03 +0530

    storing recipes ingredients also if it is an empty array

--- RecipeBook/src/app/recipes/recipe-service.ts
+++ RecipeBook/src/app/recipes/recipe-service.ts
@@ -12,10 +12,12 @@ export class RecipeService {
 
     constructor(private slService: ShoppingListsService){}
 
-    private recipes: Recipe[] = [
-        new Recipe('A test recipe', 'This is only for testing', 'https://images.unsplash.com/photo-1542010589005-d1eacc3918f2?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1192&q=80', [new Ingredient('Meat', 1), new Ingredient('bread', 2)]),
-        new Recipe('Another test recipe', 'This is only for testing', 'https://images.unsplash.com/photo-1542010589005-d1eacc3918f2?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1192&q=80', [new Ingredient('French Fries', 10), new Ingredient('Buns', 2)]),
-    ];
+    private recipes: Recipe[] = [];
+
+    // private recipes: Recipe[] = [
+    //     new Recipe('A test recipe', 'This is only for testing', 'https://images.unsplash.com/photo-1542010589005-d1eacc3918f2?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1192&q=80', [new Ingredient('Meat', 1), new Ingredient('bread', 2)]),
+    //     new Recipe('Another test recipe', 'This is only for testing', 'https://images.unsplash.com/photo-1542010589005-d1eacc3918f2?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1192&q=80', [new Ingredient('French Fries', 10), new Ingredient('Buns', 2)]),
+    // ];
 
     setRecipes(recipes: Recipe[]){
         this.recipes = recipes;
