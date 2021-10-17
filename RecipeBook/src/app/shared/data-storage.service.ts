da7f29941bdfde84c572cc0222b766a8a56a1059
Abhijitkr <abhijitkr600@gmail.com>
2021-10-17 22:06:54 +0530

    successfully storing data in Firebase realtime data storage

---
+++ RecipeBook/src/app/shared/data-storage.service.ts
@@ -0,0 +1,21 @@
+import { HttpClient } from "@angular/common/http";
+import { Injectable } from "@angular/core";
+
+import { RecipeService } from "../recipes/recipe-service";
+import { Recipe } from "../recipes/recipe.model";
+
+@Injectable({providedIn: "root"})
+export class DataStorageService {
+    constructor(private http: HttpClient, private recipeService: RecipeService) {}
+
+    storeRecipes(){
+        const recipes = this.recipeService.getRecipes();
+        this.http.put('https://learning-angular-a1274-default-rtdb.asia-southeast1.firebasedatabase.app/recipes.json', recipes)
+        .subscribe(
+            response => {
+                console.log(response);
+                
+            }
+        );
+    }
+}
